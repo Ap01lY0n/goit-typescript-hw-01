a8d544ed31d34e15e0027fc8f73f8d0b6636557a
Ap01lY0n <itstep22813@gmail.com>
2023-12-02 04:08:45 +0200

    rework

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-function add(num1: number, num2: number): void {
-	console.log(num1 + num2);
+import { concatenation } from './concatenation';
+
+const button = document.querySelector('button')!;
+const input = document.querySelector('input')!;
+
+if (button && input) {
+  button.addEventListener('click', () => {
+    concatenation(input.value, 'hello!');
+  });
 }
